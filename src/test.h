344ad9e6440acd20a5d0e021c2171f4933fc286b
Victor Yan <victor.yan889@gmail.com>
2020-05-31 21:20:37 -0400

    Format current source-code

--- src/test.h
+++ src/test.h
@@ -7,22 +7,22 @@
 namespace godot {
 
 class GDExample : public Sprite {
-    GODOT_CLASS(GDExample, Sprite)
+  GODOT_CLASS(GDExample, Sprite)
 
-private:
-    float time_passed;
+ private:
+  float time_passed;
 
-public:
-    static void _register_methods();
+ public:
+  static void _register_methods();
 
-    GDExample();
-    ~GDExample();
+  GDExample();
+  ~GDExample();
 
-    void _init(); // our initializer called by Godot
+  void _init();  // our initializer called by Godot
 
-    void _process(float delta);
+  void _process(float delta);
 };
 
-}
+}  // namespace godot
 
 #endif
