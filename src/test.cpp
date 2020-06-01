36d75dd957cb7490e215904aa60d395284112143
Victor Yan <victor.yan889@gmail.com>
2020-05-31 21:22:09 -0400

    Fix cppcheck warning

--- src/test.cpp
+++ src/test.cpp
@@ -4,7 +4,7 @@ using namespace godot;
 
 void GDExample::_register_methods() { register_method("_process", &GDExample::_process); }
 
-GDExample::GDExample() {}
+GDExample::GDExample() : time_passed{0.0} {}
 
 GDExample::~GDExample() {
   // add your cleanup here
